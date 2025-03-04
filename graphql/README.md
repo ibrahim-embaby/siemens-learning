0db78c6f8998fc0fb288e085412bc3f543a51872
Ibrahim Embaby <ibrahimembaby10@gmail.com>
2025-03-04 15:07:39 +0200

    graphql apollo server error handling

--- graphql/README.md
+++ graphql/README.md
@@ -7,3 +7,11 @@
     5- Related Data
     6- Mutations
     7- Update Mutations
+    8- Server Error Handling
+        8.1- Built-in error codes
+        8.2- Custom Errors
+        8.3- Throwing Errors
+        8.4- Including custom error details
+        8.5- Omitting or including stacktrace
+        8.6- Masking and logging errors (format error function)
+        8.7- Setting HTTP status code and headers
--- graphql/index.js
+++ graphql/index.js
@@ -1,8 +1,10 @@
 import { ApolloServer } from "@apollo/server";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerErrorCode } from "@apollo/server/errors";
 import { typeDefs } from "./schema.js";
 import db from "./_db.js";
+import { GraphQLError } from "graphql";
 
 const resolvers = {
   Query: {
@@ -10,18 +12,48 @@ const resolvers = {
       return db.games;
     },
     game(_, args) {
+      // handle in case user inputs invalid game id
+      if (args.id < 1 || args.id >= db.games.length) {
+        throw new GraphQLError("Inavalid ID", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_USER_INPUT,
+            args: "id",
+            http: {
+              status: 400,
+            },
+          },
+        });
+      }
       return db.games.find((game) => game.id === args.id);
     },
     authors() {
       return db.authors;
     },
     author(_, args) {
+      // handle in case user inputs invalid author id
+      if (args.id < 1 || args.id >= db.authors.length) {
+        throw new GraphQLError("Inavalid ID", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_USER_INPUT,
+            args: "id",
+          },
+        });
+      }
       return db.authors.find((author) => author.id === args.id);
     },
     reviews() {
       return db.reviews;
     },
     review(_, args) {
+      // handle in case user inputs invalid review id
+      if (args.id < 1 || args.id >= db.reviews.length) {
+        throw new GraphQLError("Inavalid ID", {
+          extensions: {
+            code: ApolloServerErrorCode.BAD_USER_INPUT,
+            args: "id",
+          },
+        });
+      }
       return db.reviews.find((review) => review.id === args.id);
     },
   },
@@ -51,7 +83,7 @@ const resolvers = {
     addGame(_, args) {
       let game = {
         ...args.game,
-        id: Math.floor(Math.random() * 10000).toString(),
+        id: parseInt(db.games[db.games.length - 1].id) + 1,
       };
       db.games.push(game);
 
@@ -83,6 +115,17 @@ const schema = makeExecutableSchema({
 
 const server = new ApolloServer({
   schema,
+  formatError: (formattedError, error) => {
+    return {
+      ...formattedError,
+      extensions: {
+        code: formattedError.extensions.code,
+        args: formattedError.extensions.args,
+        stacktrace: null,
+      },
+    };
+  },
+  // includeStacktraceInErrorResponses: false,
 });
 
 const PORT = 4000;
