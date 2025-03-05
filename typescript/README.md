5351849308060395b079aa85d03395014e8866f6
Ibrahim Embaby <ibrahimembaby10@gmail.com>
2025-03-05 11:45:34 +0200

    Learn TypeScript builtin types and

---
+++ typescript/README.md
@@ -0,0 +1,4 @@
+# TypeScript Progress
+
+    1- Built-In Types
+    2- The any Type
---
+++ typescript/built-in-types.ts
@@ -0,0 +1,21 @@
+/** JAVASCRIPT BUITL IN TYPES */
+// 1- string
+// 2- number
+// 3- boolean
+// 4- object
+// 5- undefined
+// 6- null
+
+/** TYPESCRIPT BUITL IN TYPES */
+// TypeScript extends JavaScript types and introduces these new types
+// 1- any
+// 2- unknown
+// 3- never
+// 4- enum
+// 5- tuple
+
+let sales: number = 123_234_332;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+
+let level; // it takes value of any
